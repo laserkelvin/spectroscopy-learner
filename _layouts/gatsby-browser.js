a314f4216bbb2361afd430af770dce0758d8fad8
Kelvin Lee <kin.long.kelvin.lee@gmail.com>
2021-11-22 10:18:49 -0500

    fix: porting over stuff that helps render math in gh-pages

---
+++ _layouts/gatsby-browser.js
@@ -0,0 +1,6 @@
+
+import 'prism-themes/themes/prism-nord.css'
+
+import 'katex/dist/katex.min.css'
+
+import './src/styles/main.css'
--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -1,20 +1,72 @@
+const path = require('path')
+
+const PATH_PREFIX = process.env.PATH_PREFIX
+
 module.exports = {
-  pathPrefix: `/foam-gatsby-template`,
+  pathPrefix: `/spectroscopy-learner`,
   siteMetadata: {
-    title: `Foam`,
-  },
-  plugins: [
+    title: `Spectroscopy Learner`,
+    author: `Kelvin Lee`,
+    description: `A Zettelkasten for learning molecular spectroscopy`,
+    social: [
+      {
+        name: `github`,
+        url: `https://github.com/laserkelvin/spectroscopy-learner`
+      },
+      {
+        name: `website`,
+        url: `https://laserkelvin.github.io`
+      }
+    ]
+  },plugins: [
     {
-      resolve: `gatsby-theme-garden`,
+      resolve: `gatsby-theme-kb`,
       options: {
-        rootNote: "/readme",
+        rootNote: '/readme',
         contentPath: `${__dirname}/..`,
         ignore: [
-          "**/_layouts/**",
-          "**/.git/**",
-          "**/.github/**",
-          "**/.vscode/**",
+          '**/_layouts/**',
+          '**/.git/**',
+          '**/.github/**',
+          '**/.vscode/**',
+          '**/.cache/**',
         ],
+        // this is an option for extending `gatsby-plugin-mdx` options inside `gatsby-theme-kb`,
+        getPluginMdx(defaultPluginMdx) {
+          // so you can have your relative referenced files served, e.g. '../assets/img.png'.
+          defaultPluginMdx.options.gatsbyRemarkPlugins.push({
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {
+              ignoreFileExtensions: ['md', 'mdx'],
+            },
+          })
+
+          // an example of syntax highlighting
+          defaultPluginMdx.options.gatsbyRemarkPlugins.push({
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              noInlineHighlight: true,
+            },
+          })
+
+          // add math support
+          defaultPluginMdx.options.remarkPlugins.push(require('remark-math'))
+          if (!defaultPluginMdx.options.rehypePlugins) defaultPluginMdx.options.rehypePlugins = []
+          defaultPluginMdx.options.rehypePlugins.push(require('rehype-katex'))
+          return defaultPluginMdx
+        },
+      },
+    },
+    {
+      // this plugin makes sure your static files will be served by gatsby,
+      //   but of course you need to reference them by absolute path, e.g. '/assets/img.png'.
+      // if you have multiple directories, copy this plugin section and specify other directory
+      // check https://github.com/csath/gatsby-plugin-copy-files-enhanced to find docs for this plugin
+      resolve: 'gatsby-plugin-copy-files-enhanced',
+      options: {
+        source: path.resolve(__dirname, `../assets`),
+        destination: '/assets',
+        purge: false,
       },
     },
   ],
